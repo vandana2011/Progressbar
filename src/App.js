6c13568410e4b09f59e4d1b7f44e12fa6a12762f
vandana2011 <vandana20.prajapati@gmail.com>
2025-03-24 12:15:32 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -4,12 +4,10 @@ import { useState } from 'react';
 
 
 function App() {
-const [progress, setProgress] = useState(99)
+const [progress, setProgress] = useState(1)
 
 const increment = (() => {  
-  setProgress(progress + 1)
-
- 
+  setProgress(progress + 1) 
 })
 const dicrement = (()=>{
   setProgress(progress - 1)
@@ -19,13 +17,18 @@ const dicrement = (()=>{
     <>
    
    {/* <input type='progress'/> */}
+ 
    <progress id="file" value={progress} max="100" min="0">  </progress>
     <span>{progress}%</span>
-
+    <div>
     <button onClick={increment}>
-    increment
-    </button>
-    <button onClick={dicrement}> dicrement</button>
+        increment
+        </button>
+        <button onClick={dicrement}> dicrement</button>
+    </div>
+   
+   
+  
     </>
   );
 }
